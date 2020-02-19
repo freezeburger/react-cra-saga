0cba5b2eb77a865e38a9ad6efd68d3dc454a5d61
freezeburger <renaud.dubuis@decryptage.net>
2020-02-19 01:28:30 +0100

    Deep Context

--- src/App.js
+++ src/App.js
@@ -2,51 +2,70 @@ import React from 'react';
 import ApiProviderConnector from './StoredApplicationLogic';
 import { ReactReduxContext } from 'react-redux';
 
+const Level1 = ({ children }) => <div>{children}</div>;
+const Level2 = ({ children }) => <div>{children}</div>;
+const Level3 = ({ children }) => <div>{children}</div>;
+const Level4 = ({ children }) => <div>{children}</div>;
+
 const HiJackingStoreComponent = () => {
   return (
     <ReactReduxContext.Consumer>
       {({ store }) => {
-        return (
-          <fieldset>
-          {store.getState().time.currentFileTime}
-          </fieldset>
-        )}}
+        return <fieldset>{store.getState().time.currentFileTime}</fieldset>;
+      }}
     </ReactReduxContext.Consumer>
   );
-}
+};
 
-class App extends React.Component{
-
-  // Explicit but not mandatory 
-  constructor(props){
+class App extends React.Component {
+  // Explicit but not mandatory
+  constructor(props) {
     super(props);
     console.log(props);
   }
 
   // http://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
-  componentDidMount(){
+  componentDidMount() {
     console.log(this.props);
   }
-  componentDidUpdate(){}
+  componentDidUpdate() {}
 
-  componentWillUnmount(){}
+  componentWillUnmount() {}
 
   render() {
-    return <div className="App">{this.props.polling && <div>Fecthing Data</div>}{this.props.time.currentFileTime}<HiJackingStoreComponent/></div>;
+    return (
+      <div className="App">
+        {this.props.polling && <div>Fecthing Data</div>}
+        {this.props.time.currentFileTime}
+        <Level1>
+          <Level2>
+            <Level3>
+              <Level4>
+                <HiJackingStoreComponent />
+              </Level4>
+            </Level3>
+          </Level2>
+        </Level1>
+      </div>
+    );
   }
-
 }
 
 // this function should be named a selector as it helps the component selecting data from the state
-const mapApplicationStateToComponentProps = (state) => {
-  console.log("App State ->", state);
+const mapApplicationStateToComponentProps = state => {
+  console.log('App State ->', state);
   return {
     value: state.defaultValue,
-    time:state.time,
-    polling:state.polling
-  }
-}
+    time: state.time,
+    polling: state.polling
+  };
+};
 
 // Connecting The App through a the react-redux Provider
-const ConnectedApp = () => <ApiProviderConnector mapper={mapApplicationStateToComponentProps} Component={App}></ApiProviderConnector>;
+const ConnectedApp = () => (
+  <ApiProviderConnector
+    mapper={mapApplicationStateToComponentProps}
+    Component={App}
+  ></ApiProviderConnector>
+);
 export default ConnectedApp;
