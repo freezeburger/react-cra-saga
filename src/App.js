685a7a1d1f28d863f92791a9795cc76c6e07e7e3
freezeburger <renaud.dubuis@decryptage.net>
2020-02-21 13:44:39 +0100

    Init

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import ApiProviderConnector, {
+  ApiProviderUnsafeContext
+} from './StoredApplicationLogic';
+import { ReactReduxContext } from 'react-redux';
+
+const Level1 = ({ children }) => <div>{children}</div>;
+const Level2 = ({ children }) => <div>{children}</div>;
+const Level3 = ({ children }) => <div>{children}</div>;
+const Level4 = ({ children }) => <div>{children}</div>;
+
+const HiJackingStoreComponent = () => {
+  return (
+    <ReactReduxContext.Consumer>
+      {(contextValue = {}, { store } = contextValue) => {
+        return <fieldset>{store.getState().time.currentFileTime}</fieldset>;
+      }}
+    </ReactReduxContext.Consumer>
+  );
+};
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        {/* 
+        {this.props.polling && <div>Fecthing Data</div>}
+        {this.props.time.currentFileTime}
+         */}
+        <Level1>
+          <Level2>
+            <Level3>
+              <Level4>
+                <HiJackingStoreComponent />
+              </Level4>
+            </Level3>
+          </Level2>
+        </Level1>
+        <ApiProviderUnsafeContext>
+          <HiJackingStoreComponent />
+        </ApiProviderUnsafeContext>
+      </div>
+    );
+  }
+}
+
+const mapApplicationStateToComponentProps = state => {
+  return {
+    value: state.defaultValue,
+    time: state.time,
+    polling: state.polling
+  };
+};
+
+// Connecting The App through a the react-redux Provider
+const ConnectedApp = () => (
+  <ApiProviderConnector
+    mapper={mapApplicationStateToComponentProps}
+    Component={App}
+  ></ApiProviderConnector>
+);
+export default ConnectedApp;
---
+++ src/StoreApplicationSaga.js
@@ -0,0 +1,33 @@
+import createSagaMiddleware from 'redux-saga';
+import { put, all, take, call, delay } from 'redux-saga/effects'
+
+// Saga Glossary : https://redux-saga.js.org/docs/Glossary.html
+
+const TIME = 'http://worldclockapi.com/api/json/utc/now';
+const fetchTime = () => {
+    console.warn('API CALL')
+    return fetch(TIME).then(res => res.json())
+}
+
+/* Worker Function */
+function* updateTime() {
+    while (true) {   
+        yield put({type: '@@POLING_TIME_START' });
+        const data = yield call(fetchTime);
+        yield put({type: '@@POLING_TIME_END', data });
+        yield delay(5000)
+        yield put({type: '@@POLING_TIME_RETRY' });
+    }
+}
+
+function* rootSagaWatcher() {
+    yield take('INIT_SAGA_WORKERS');
+    yield all([
+        updateTime()
+    ]);
+}
+
+const timeSagaMiddleware = createSagaMiddleware();
+const timeSagaRun = () => timeSagaMiddleware.run(rootSagaWatcher);
+
+export {timeSagaMiddleware,timeSagaRun};
